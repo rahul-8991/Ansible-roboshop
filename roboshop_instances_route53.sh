e22defb12ea53b19eadbc473f2dba61eaf39176e
unknown <kudikyalarahul8055@gmail.com>
2026-08-20 13:43:44 +0530

    Update roboshop instance script

--- roboshop_instances_route53.sh
+++ roboshop_instances_route53.sh
@@ -2,39 +2,76 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-016466050ba240b84" # Allow all traffic SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z0176514X7F0V5F420IF" # aws route53 cone ID
-DOMAIN_NAME="rahuldaws.store" # roboshop website DNS
+ZONE_ID="Z071722410SPRZ7TY4PCQ" # AWS Route53 hosted zone ID
+DOMAIN_NAME="rahuldaws.store" # Roboshop website DNS
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in "$@"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type t2.micro --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ $instance != "frontend" ]
+    # Check if instance already exists
+    INSTANCE_ID=$(aws ec2 describe-instances \
+        --filters \
+        "Name=tag:Name,Values=$instance" \
+        "Name=instance-state-name,Values=pending,running,stopping,stopped" \
+        --query "Reservations[].Instances[].InstanceId" \
+        --output text)
+
+    if [ -z "$INSTANCE_ID" ]
     then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+        echo "$instance instance does not exist. Creating..."
+
+        INSTANCE_ID=$(aws ec2 run-instances \
+            --image-id "$AMI_ID" \
+            --instance-type t2.micro \
+            --security-group-ids "$SG_ID" \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+            --query "Instances[0].InstanceId" \
+            --output text)
+
+        echo "$instance instance created: $INSTANCE_ID"
+
+        # Wait until instance is running
+        aws ec2 wait instance-running \
+            --instance-ids "$INSTANCE_ID"
+
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        echo "$instance instance already exists: $INSTANCE_ID"
+    fi
+
+    if [ "$instance" != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query "Reservations[0].Instances[0].PrivateIpAddress" \
+            --output text)
+
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query "Reservations[0].Instances[0].PublicIpAddress" \
+            --output text)
+
         RECORD_NAME="$DOMAIN_NAME"
     fi
+
     echo "$instance IP address: $IP"
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
+        --hosted-zone-id "$ZONE_ID" \
+        --change-batch '{
+            "Comment": "Creating or Updating a record set",
+            "Changes": [{
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'"$RECORD_NAME"'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [{
+                        "Value": "'"$IP"'"
+                    }]
+                }
             }]
-        }
-        }]
-    }'
+        }'
+
 done
